e9daf2aa96d85ead012c7504c0f7cf088466a808
czy0724 <czy20060724@outlook.com>
2025-02-28 10:29:01 +0800

    Add files via upload

---
+++ pizza2/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    private void printReceipt() {
+        System.out.println("********RECEIPT********");
+        System.out.println("Order ID: " + orderID);
+        System.out.println("Order Details: " + pizzaIngredients);
+        System.out.println("Order Total: " + orderTotal);
+    }
+}  
